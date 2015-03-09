f514eb9b0c79d9cdb8e2e6d3b80aa3184fcbcbf5
Spencer Ruskin <ruskin@gmail.com>
2015-03-09 14:11:23 -0700

    added bundler, added breakpoints, updated styles, added reset button

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ $(document).ready(init);
 function init()
 {
   console.log('init');
-  $('.puzzle-cell').on('click',cellClicked);
+  // $('.puzzle-cell').on('click',cellClicked);
   $('#swap').on('click',swapModes);
-  $('#solve').on('click',validateSolution);
+  $('#reset').on('click',resetPuzzle);
   $('.puzzle-cell').not('.initial').each(function(){
     $(this).qtip(
       {
@@ -40,11 +40,6 @@ function init()
   });
 }
 
-function cellClicked(e)
-{
-  console.log($(e.currentTarget).data());
-}
-
 function swapModes(e)
 {
   if ($(e.currentTarget).data('mode') == 'numbers')
@@ -59,14 +54,100 @@ function swapModes(e)
   }
 }
 
+function resetPuzzle()
+{
+  $('.puzzle-cell').not('.initial').attr('data-val','0').data('val','0')
+}
+
 function tipClicked(swatch,tip)
 {
-  console.log(tip);
-  tip.target.attr('data-val',$(swatch).data('val'));
+  tip.target.attr('data-val',$(swatch).data('val')).data('val',$(swatch).data('val'));
+  checkCompletion();
   tip.hide();
+}
+
+function checkCompletion()
+{
+  if (!$('.puzzle-cell[data-val="0"]').length)
+  {
+    $('#solve').removeClass('disabled').on('click',validateSolution);
+  }
 }
 
 function validateSolution(e)
 {
-  console.log('validate');
+  console.log('valid puzzle?');
+  var valid = true;
+  $('.puzzle-cell').each(function()
+    {
+      if (!validateCell($(this)))
+      {
+        valid = false;
+      }
+    }
+  );
+  if (valid)
+  {
+    alert('You solved it!');
+  }
+  else
+  {
+    alert('Sorry, not a winner. Please try again.');
+  }
+}
+
+function validateCell(cell)
+{
+  var matrixValues = getMatrixVals(cell);
+  var rowValues = getRowVals(cell);
+  var colValues = getColVals(cell);
+  return (isUnique(matrixValues) && isUnique(rowValues) && isUnique(colValues));
+}
+
+function getMatrixVals(cell)
+{
+  var values = [];
+  cell.siblings().andSelf().each(function()
+    {
+      values.push($(this).data('val'));
+    }
+  );
+  return values;
+}
+
+function getRowVals(cell)
+{
+  var values = [];
+  var rowMembers = $('.puzzle-cell[data-row="'+cell.data('row')+'"');
+  rowMembers.each(function()
+    {
+      values.push($(this).data('val'));
+    }
+  );
+  return values;
+}
+
+function getColVals(cell)
+{
+  var values = [];
+  var colMembers = $('.puzzle-cell[data-col="'+cell.data('col')+'"');
+  colMembers.each(function()
+    {
+      values.push($(this).data('val'));
+    }
+  );
+  return values;
+}
+
+function isUnique(testArray)
+{
+  var map = {};
+  for(var i = 0;i < testArray.length;i++)
+  {
+    if (!map[testArray[i]])
+    {
+      map[testArray[i]] = true;
+    }
+  }
+  return (Object.keys(map).length === testArray.length);
 }
